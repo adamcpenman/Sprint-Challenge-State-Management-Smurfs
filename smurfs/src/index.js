81ac426d5dad4b298c8799208fc5e6eae4bb7234
Your Name <you@example.com>
2019-10-26 13:17:30 -0500

    MVP

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { Provider } from "react-redux";
+import {reducer} from "./reducers/index";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+
+ReactDOM.render(<Provider store={store}>
+    <App />
+  </Provider>, document.getElementById("root"));
